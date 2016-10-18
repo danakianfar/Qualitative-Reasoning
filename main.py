c01e2a221487994b11f89920bfc8f62a63a059aa
jgalle29 <jgalle29@gmail.com>
2016-10-18 15:43:12 +0200

    debug prune

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import numpy as np
 import matplotlib.pyplot as plt
 import itertools
 from state_graph import *
@@ -6,14 +7,14 @@ from sets import Set
 # Variable ordering [I V H P O]
 
 # 0, plus, max domain for all the variables
-var_dom = [0, 1, 2]
+var_dom = [0.0, 1.0, 2.0]
 
 # ?, negative, 0, plus for all derivatives
-der_dom = [-100,-1,0,1]
+der_dom = [-9.0,-1.0,0.0,1.0]
 
 # creates full envisionment
 st_var = list(itertools.product(var_dom, repeat=5))
-st_der = list(itertools.product(der_dom, repeat=5))
+st_der = list(itertools.product([-1,0,1], [-9,-1,0,1], [-9,-1,0,1], [-9,-1,0,1], [-9,-1,0,1]))
 states = list(itertools.product(st_var, st_der))
 
 # translates envisionment to np array
@@ -22,59 +23,75 @@ for i in range(len(states)):
     S.append( np.asarray(states[i][0] + states[i][1]))
 S = np.asarray(S)
 
+# Matrix of influence dependencies (row influences column): +1 I+, -1 I-, 0 no relation
+I = np.array([[0, 1, 0, 0, 0],
+              [0, 0, 0, 0, 0],
+              [0, 0, 0, 0, 0],
+              [0, 0, 0, 0, 0],
+              [0, -1, 0, 0, 0]])
 
-S = prune_states(S)
+# Matrix of proportionality dependencies (row influences column): +1 I+, -1 I-, 0 no relation
+P = np.array([[0, 0, 0, 0, 0],
+              [0, 0, 1, 0, 0],
+              [0, 0, 0, 1, 0],
+              [0, 0, 0, 0, 1],
+              [0, 0, 0, 0, 0]])
+
+S = prune_states(S,I)
 
 print S
 
-G, T = create_graph(S)
 
-print ' -----'
+###
 
-s1 = Set([])
-s2 = Set([])
-s0 = Set([])
-
-for t in T:
-    s0.add(str(t.origin))
-    s0.add(str(t.destination))
-
-    s1.add(str(t.origin))
-    s2.add(str(t.destination))
-    #print t.prettyprint()
-
-print 'Total: ' + str(len(s0))
-print 'Origin: ' + str(len(s1))
-print 'Destinations: ' + str(len(s2))
-print 'Transitions: ' + str(len(T))
-
-print ' **** '
-
-for s in s0:
-    print s
-
-print ' **** '
-
-#zstate = np.array([0, 0, 0, 0, 0, 0, 0, 0, 0, 0])
-zstate = np.array([0, 0, 0, 0, 0, 1, 0, 0, 0, 0])
-
-
-sstate=(np.array([   1 ,   0,    0,    0,    0,    1 ,   1 ,   1 ,   1 ,   1]))
-
-tr = Transition(zstate,sstate)
-print tr.checkValidity()
-
-pos = nx.fruchterman_reingold_layout(G)
-
-print G.selfloop_edges()
-
-for v in G.nodes():
-    G.node[v]['state']=v
-
-nx.draw(G, pos)
-node_labels = nx.get_node_attributes(G,'state')
-nx.draw_networkx_labels(G, pos, labels = node_labels)
-#plt.savefig('this.png')
-plt.show()
-
-
+# G, T = create_graph(S)
+#
+# print ' -----'
+#
+# s1 = Set([])
+# s2 = Set([])
+# s0 = Set([])
+#
+# for t in T:
+#     s0.add(str(t.origin))
+#     s0.add(str(t.destination))
+#
+#     s1.add(str(t.origin))
+#     s2.add(str(t.destination))
+#     #print t.prettyprint()
+#
+# print 'Total: ' + str(len(s0))
+# print 'Origin: ' + str(len(s1))
+# print 'Destinations: ' + str(len(s2))
+# print 'Transitions: ' + str(len(T))
+#
+# print ' **** '
+#
+# for s in s0:
+#     print s
+#
+# print ' **** '
+#
+# #zstate = np.array([0, 0, 0, 0, 0, 0, 0, 0, 0, 0])
+# zstate = np.array([0, 0, 0, 0, 0, 1, 0, 0, 0, 0])
+#
+#
+# sstate=(np.array([   1 ,   0,    0,    0,    0,    1 ,   1 ,   1 ,   1 ,   1]))
+#
+# tr = Transition(zstate,sstate)
+# print tr.checkValidity()
+#
+# pos = nx.fruchterman_reingold_layout(G)
+#
+# print G.selfloop_edges()
+#
+# for v in G.nodes():
+#     G.node[v]['state']=v
+#
+# nx.draw(G, pos)
+# node_labels = nx.get_node_attributes(G,'state')
+# nx.draw_networkx_labels(G, pos, labels = node_labels)
+# #plt.savefig('this.png')
+# plt.show()
+#
+#
--- state_graph.py
+++ state_graph.py
@@ -1,42 +1,82 @@
 import numpy as np
 import networkx as nx
 from transition import Transition
+from sets import Set
 
-def prune_states(S):
+def prune_states(S,I):
     # number of variables
     nvars = np.shape(S)[1]/2
+    init_states = len(S)
     print 'Initial number of states: ' + str(len(S))
     #stores the states to be deleted
-    del_states = []
+    del_states = Set([])
     for s_ix in range(len(S)):
         s = S[s_ix]
-        # exogenous variable can not have ambiguous derivative
-        if s[nvars] < -1:
-            del_states.append(s_ix)
-            continue
         for i in range(nvars):
             # if max value then can not have positive derivative
             if s[i] == 2 and s[i+nvars] == 1:
-                del_states.append(s_ix)
+                del_states.add(s_ix)
                 break
             #if min value then can not have negative derivative
             if s[i] == 0 and s[i+nvars] == -1:
-                del_states.append(s_ix)
+                del_states.add(s_ix)
                 break
-        # checks for positive influence
-        if not( (s[1+nvars] != 1) or  (s[0] >0)) or ((s[0] == 0) and (s[1+nvars] < -1)):
-            del_states.append(s_ix)
-            continue
+
         # checks for equivalent variables V H P O
-        for i in range(1,nvars):
-            for j in range(i+1,nvars):
+        for i in range(1, nvars):
+            for j in range(1, nvars):
                 if (s[i] != s[j]) or (s[i + nvars] != s[j + nvars]):
-                    del_states.append(s_ix)
+                    del_states.add(s_ix)
                     break
 
-    print 'Number of states prunned: ' + str(len(del_states))
+
+    S = np.delete(S, list(del_states), axis=0)
+
+    for ix in range(len(S)):
+        print str(ix) + ' -- ' + str(S[ix,[0,1,5,6]])
+
+    del_states1 = Set([])
+    del_states2 = Set([])
+    del_states3 = Set([])
+
+    for s_ix in range(len(S)):
+        s = S[s_ix]
+        # checks for influence relationships
+        for j in range(nvars):
+            influences = Set([])
+
+            for i in range(nvars):
+                if I[i][j] != 0 and s[i] != 0:
+                    influences.add(I[i][j])
+
+            #if len(influences) == 0 and s[j+nvars] != 0:
+            #    del_states1.add(s_ix)
+            #    continue
+
+            #mixed signs
+            if len(influences) == 2 and not(s[j+nvars] < -1):
+                del_states2.add(s_ix)
+                continue
+
+            #same sign
+            if len(influences) == 1 and influences.pop() != s[j + nvars]:
+                del_states3.add(s_ix)
+                continue
+
+    print '***********'
+
+    print list(del_states1)
+    print list(del_states2)
+    print list(del_states3)
+    print '+++++++++++'
+
+    del_states = del_states1 | del_states2 | del_states3
+    print S[list(del_states)]
+    print '***********'
+
     # deletes the invalid states
-    S = np.delete(S,del_states, axis=0)
+    S = np.delete(S,list(del_states), axis=0)
+    print 'Number of states prunned: ' + str(init_states-len(S))
     print 'Final number of states: ' + str(len(S))
     return S
 
